6f1b3bcfba559c2cbf9ba434c1d460a2a696b000
Trigon27 <ashrafshaikh0778866@gmail.com>
2024-05-07 20:46:31 +0530

    client side initial files

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is a Tailwind CSS example.
+    </div>
+  );
+}
+
+export default App;
